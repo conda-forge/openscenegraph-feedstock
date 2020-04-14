8a244b044fb6e57513c43bf78e5f138d898ff48f
Justin Carpentier <justin.carpentier@inria.fr>
2020-04-14 10:33:21 +0200

    Add Windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig
+
+mkdir build
+cd build
+
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
