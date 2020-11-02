1fdcdb09bf46299ac997c6c461ecd4858356bc5b
yann_dm <yann.montmarin@gmail.com>
2020-11-02 17:18:31 +0100

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,6 @@ cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-
+      -DCMAKE_INSTALL_LIBDIR=lib
 make -j${CPU_COUNT}
 make install
-
