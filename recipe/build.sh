db051258335619e67ac346b2ad22e4ff2073dd56
Silvio Traversaro <silvio@traversaro.it>
2022-05-21 15:26:53 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ cd build
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${target_platform}" == "osx-arm64" ]]; then
   # Need to set these explicitly for osx-arm64 since they can't be checked
   # automatically when cross-compiling, see https://github.com/conda-forge/openscenegraph-feedstock/pull/18#issuecomment-1133066499
-  export CMAKE_ARGS="${CMAKE_ARGS} -D_OPENTHREADS_ATOMIC_USE_BSD_ATOMIC_EXITCODE=0 -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE=0"
+  export CMAKE_ARGS="${CMAKE_ARGS} -D_OPENTHREADS_ATOMIC_USE_BSD_ATOMIC_EXITCODE__TRYRUN_OUTPUT=\"\" -D_OPENTHREADS_ATOMIC_USE_BSD_ATOMIC_EXITCODE=0 -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE__TRYRUN_OUTPUT=\"\" -D_OPENTHREADS_ATOMIC_USE_GCC_BUILTINS_EXITCODE=0"
+  echo "Set CMAKE_ARGS To ${CMAKE_ARGS}"
 fi
 
 cmake ${CMAKE_ARGS} .. \
